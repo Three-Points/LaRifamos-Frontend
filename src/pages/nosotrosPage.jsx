f9fcdaddd259c6cd1c2669d08750d71b62f15dcd
Manuel <manuel@martinezcasas.com>
2022-09-17 23:32:08 -0500

    new(aboutUs.jsx) nuevo aboutUs

---
+++ src/pages/nosotrosPage.jsx
@@ -0,0 +1,33 @@
+
+
+function AboutUs(){
+
+
+    return (
+        <>
+            <div class = "container">
+                
+                <div class="row m-5">
+
+                    <h1> Nosotros </h1>
+
+                </div>
+            
+
+                <div class="row m-5">
+
+                    <p class = "body"> LaRifamos es una plataforma web,digital y moderna que permite rifar tus productos, 
+                        obteniendo mejores beneficios con respecto a los canales de venta tradicionales. </p>
+                    
+                </div>
+
+
+            </div>
+
+        </>
+
+    )
+}
+
+
+export default AboutUs; 
