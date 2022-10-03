7fbf27c8e7dd672715edc521f1c5aa86ad565b54
pepcerda <josep.cerda.roscar@gmail.com>
2022-10-03 20:41:08 +0200

    #33 FilterButton Component

---
+++ src/pages/rifasPage/rifasComponents/filterButton.jsx
@@ -0,0 +1,11 @@
+
+function FilterButton() {
+    return (
+        <div class="mx-5">
+            <a href="" class="mx-5 rifas-btn d-flex align-content-center justify-content-center"><img
+                src="./imgs/filtros.svg" alt="" /><span>Filtros</span></a>
+        </div>
+    );
+}
+
+export default FilterButton; 
