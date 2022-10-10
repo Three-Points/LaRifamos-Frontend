f6f386d4a46810f9047a178df22a911181565a3a
pepcerda <josep.cerda.roscar@gmail.com>
2022-10-10 23:19:12 +0200

    #13 Creation of Raffles Page

--- src/pages/rifasPage/rafflesPage.jsx
+++ src/pages/rifasPage/rafflesPage.jsx
@@ -1,28 +1,54 @@
 import data from '../../data/rifasPage.mock.json';
-import { createContext, useState } from "react";
-import SearchButton from "./rifasComponents/searchButton";
-import RafflesList from "./rifasComponents/rafflesList";
-import RafflesFilters from "./rifasComponents/rafflesFilters";
+import { createContext, useState, useEffect } from 'react';
+import SearchButton from './rifasComponents/searchButton';
+import RafflesList from './rifasComponents/rafflesList';
+import RafflesFilters from './rifasComponents/rafflesFilters';
+import { getRaffles } from '../../services/raffles.service';
 
 export const CategoryCardContext = createContext();
+export const SearchRaffleContext = createContext(); 
 
 function RafflesPage() {
+    const [search, setSearch] = useState('');
+    const [categoryCard, setCategoryCard] = useState('');
+    const [raffles, setRaffles] = useState(""); 
+    const [showRaffles, setShowRaffles] = useState(false); 
 
-    const raffles = data.raffles;
-    const [search, setSearch] = useState("");
-    const [categoryCard, setCategoryCard] = useState("");
+    useEffect(() => {
+        console.log("Entro")
+        getRaffles().then((response) => {
+            console.log(response.data);
+            let raffles_obtained = {
+                "raffles": response.data
+            }
+            console.log(raffles_obtained); 
+            setRaffles(raffles_obtained.raffles); 
+            setShowRaffles(true); 
+        }).catch(() => {
+            //Show error
+        })
+    }, [])
 
     return (
         <>
-            <SearchButton onSearch={(str) => setSearch(str)}></SearchButton>
-            <CategoryCardContext.Provider value={{categoryCard, setCategoryCard}}>
-                <RafflesFilters></RafflesFilters>
-                <RafflesList raffles={raffles} search={search}></RafflesList>
-            </CategoryCardContext.Provider>        
+            <CategoryCardContext.Provider
+                value={{ categoryCard, setCategoryCard }}
+            >
+                <SearchRaffleContext.Provider value={{ search, setSearch }}>
+                    <RafflesFilters></RafflesFilters>
+                    {showRaffles ? (
+                        <RafflesList
+                        raffles={raffles}
+                        search={search}
+                    ></RafflesList>
+                    ):(
+                        <></>
+                    )}
+                    
+                </SearchRaffleContext.Provider>
+            </CategoryCardContext.Provider>
         </>
-    );
+    )
 }
 
-export default RafflesPage;
-
-
+export default RafflesPage
