ac33c8069edcafa9bd026d2caf28e825ce0e00e4
pepcerda <josep.cerda.roscar@gmail.com>
2022-10-10 23:14:45 +0200

    #14 Search Component Creation

--- src/pages/rifasPage/rifasComponents/searchButton.jsx
+++ src/pages/rifasPage/rifasComponents/searchButton.jsx
@@ -1,24 +1,27 @@
-function SearchButton({onSearch}) {
+import { useContext } from "react";
+import { SearchRaffleContext } from "../rafflesPage";
+
+function SearchButton() {
+
+    const {search , setSearch} = useContext(SearchRaffleContext); 
 
     let handleKeyDown = (e) => {
         let input = document.getElementById("rifas-search"); 
         if (e.key === 'Enter') {
-            onSearch(input.value); 
+            setSearch(input.value); 
         }
     }
 
     let handleClick = (e) => {
         let input = document.getElementById("rifas-search"); 
-        onSearch(input.value); 
+        setSearch(input.value); 
     }
 
     return (
-        <div className="mb-5 order-3 order-md-2 rifas-search-container">
             <div className="rifas-search px-3 d-flex align-content-center align-items-center">
                 <input id="rifas-search" className="mx-2" type="text" name="" placeholder="Buscar productos..."  onKeyDown={handleKeyDown}/>
                 <span className="rifas-search-button d-flex align-items-center justify-content-center" onClick={handleClick}><img src="./imgs/Lupa.svg" alt="" className="img-lupa" /></span>
             </div>
-        </div>
     );
 }
 
---
+++ src/pages/rifasPage/rifasComponents/searchButtonNavbar.jsx
@@ -0,0 +1,12 @@
+import SearchButton from "./searchButton";
+
+function SearchButtonNavbar () {
+
+    return (
+        <div className="mb-5 order-3 order-md-2 rifas-search-container">
+            <SearchButton></SearchButton>
+        </div>
+    )
+}
+
+export default SearchButtonNavbar; 
