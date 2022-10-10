8f4d88245e603ef9d185cb8a79931723cac096ae
pepcerda <josep.cerda.roscar@gmail.com>
2022-10-10 23:16:29 +0200

    #33 RaffleFilter Card Component

--- src/pages/rifasPage/rifasComponents/filterCard.jsx
+++ src/pages/rifasPage/rifasComponents/filterCard.jsx
@@ -1,20 +1,35 @@
-import { useContext} from "react";
-import { CategoryCardContext } from "../rafflesPage";
+import { useContext, useEffect, useState } from 'react'
+import { CategoryCardContext } from '../rafflesPage'
+import { ActiveFilterContext } from './rafflesFilters'
 
-function FilterCard({title, image}) {
-    const {categoryCard, setCategoryCard} = useContext(CategoryCardContext);
+function FilterCard({ title, image }) {
+    const { categoryCard, setCategoryCard } = useContext(CategoryCardContext)
+    const {activeFilter, setActiveFilter} = useContext(ActiveFilterContext); 
 
-    //TODO -> handleClick: revisar que si se clica siendo activo o no el elemento. En caso de segundo click en activo, se quita el filtro de categoria. 
-    
+    const handleClick = (title_card) => {
+        console.log(title, activeFilter); 
+        if(activeFilter !== title) {
+            setCategoryCard({title});
+            setActiveFilter(title_card);
+            console.log("entro aqui")
+        } else {
+            setActiveFilter('');
+            setCategoryCard(''); 
+        }
+          
+    }
 
     return (
-        <div className="d-flex justify-content-center flex-column text-center">
-            <div className="category-filter d-flex flex-column align-items-center justify-content-center" onClick={() => setCategoryCard({title})}>
+        <div className="category-filter-container d-flex justify-content-center flex-column text-center">
+            <div
+                className={`${activeFilter.includes(title) ? 'category-filter-active': 'category-filter'}  d-flex flex-column align-items-center justify-content-center`}
+                onClick={() => {handleClick(title)}}
+            >
                 <img src={image} alt="" />
             </div>
+
             <p>{title}</p>
         </div>
-
     )
 }
-export default FilterCard; 
+export default FilterCard
