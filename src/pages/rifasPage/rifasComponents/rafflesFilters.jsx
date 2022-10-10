a5521adf3d24aacff37a6829c0c60e06083abd6b
pepcerda <josep.cerda.roscar@gmail.com>
2022-10-10 23:17:32 +0200

    #35 RafflesFilter Component

--- src/pages/rifasPage/rifasComponents/rafflesFilters.jsx
+++ src/pages/rifasPage/rifasComponents/rafflesFilters.jsx
@@ -1,24 +1,59 @@
-import FilterButton from "./filterButton";
-import data from '../../../data/filters.json';
-import FilterCard from "./filterCard";
+import FilterButton from './filterButton'
+import data from '../../../data/filters.json'
+import FilterCard from './filterCard'
+import SearchButton from './searchButton'
+import SearchButtonNavbar from './searchButtonNavbar'
+import { createContext, useContext, useState } from 'react'
+import { ViewWidthContext } from '../../../App'
+import FilterSideMenu from './filterSideMenu'
+
+export const SideMenuFilterContext = createContext();
+export const ActiveFilterContext = createContext();  
 
 function RafflesFilters() {
-    const filters = data.filters;
+    const filters = data.filters
 
+    const { viewWidth, setViewWidth } = useContext(ViewWidthContext);
+    const [showSideMenuFilter, setShowSideMenuFilter] = useState(false);
+    const [activeFilter, setActiveFilter] = useState("");
 
     return (
         <>
-            <div class="container d-flex justify-content-between align-items-center mt-4">
-                <div class="category-container d-flex mx-4">
-                    {filters.map((filter) => {
-                        return <FilterCard title={filter.title} image={filter.image}
-                                ></FilterCard>
-                    })}
-                </div>
-                <FilterButton></FilterButton>
-            </div>
+            <SideMenuFilterContext.Provider
+                value={{ showSideMenuFilter, setShowSideMenuFilter }}
+            >
+                <ActiveFilterContext.Provider value={{activeFilter, setActiveFilter}}>
+                {showSideMenuFilter ? (
+                    <FilterSideMenu
+                        filters={filters}
+                    ></FilterSideMenu>
+                ) : (
+                    <></>
+                )}
+                {viewWidth >= 768 ? (
+                    <>
+                        <SearchButtonNavbar></SearchButtonNavbar>
+                        <div className="container d-flex justify-content-between align-items-center mt-4">
+                            <div className="category-container d-flex flex-wrap">
+                                {filters.map((filter) => {
+                                    return (
+                                        <FilterCard
+                                            title={filter.title}
+                                            image={filter.image}
+                                        ></FilterCard>
+                                    )
+                                })}
+                            </div>
+                            <FilterButton></FilterButton>
+                        </div>
+                    </>
+                ) : (
+                    <FilterButton></FilterButton>
+                )}
+                </ActiveFilterContext.Provider>
+            </SideMenuFilterContext.Provider>
         </>
-    );
+    )
 }
 
-export default RafflesFilters; 
+export default RafflesFilters;
